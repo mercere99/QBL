568677962a4a5d3d78701a47b9bbc12331a70f15
Charles Ofria <charles.ofria@gmail.com>
2023-03-08 09:15:14 -0500

    Added an AddTags() function to Question class.

--- Question.hpp
+++ Question.hpp
@@ -15,6 +15,10 @@ private:
   emp::vector<emp::String> options;
   size_t correct = (size_t) -1;
   size_t id = (size_t) -1;
+
+  emp::vector<String> base_tags;
+  emp::vector<String> exclusive_tags;
+  std::map<String,String> config_tags;
 public:
   Question(size_t _id) : id(_id) { };
   Question(const Question &) = default;
@@ -38,6 +42,23 @@ public:
     options.push_back(option);
   }  
 
+  void AddTags(String line) {
+    line.Compress();
+    auto tags = line.Slice(" ");
+    for (auto tag : tags) {      
+      if (tag[0] == '#') base_tags.push_back(tag);
+      else if (tag[0] == '^') exclusive_tags.push_back(tag);
+      else if (tag[0] == ':') {
+        emp::notify::TestError(!tag.Has('='), "Tag '", tag, "' must have an assignment.");
+        String name = tag.Pop('=');
+        emp::notify::TestError(tag.size() == 0, "Tag '", tag, "' must have value after '='.");
+        config_tags[name] = tag;
+      }
+      else {
+        emp::notify::Error("Unknown tag type '", tag, "'.");
+      }
+    }
+  }
   void Print(std::ostream & os=std::cout) const;
   void PrintD2L(std::ostream & os=std::cout) const;
 };
