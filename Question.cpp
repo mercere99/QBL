e573006c4864846c0f78fb3c71452821f8511492
Charles Ofria <charles.ofria@gmail.com>
2023-03-09 18:20:51 -0500

    Lots of new Question functionality.

--- Question.cpp
+++ Question.cpp
@@ -1,20 +1,19 @@
 #include "Question.hpp"
+#include "functions.hpp"
+
+using emp::MakeCount;
 
 void Question::Print(std::ostream & os) const {
   os << "%- QUESTION " << id << "\n" << question << "\n";
   for (size_t opt_id = 0; opt_id < options.size(); ++opt_id) {
-    if (opt_id == correct) os << "[*] ";
+    if (options[opt_id].is_correct) os << "[*] ";
     else os << "* ";
-    os << options[opt_id] << '\n';
+    os << options[opt_id].text << '\n';
   }
   os << std::endl;
 }
 
 void Question::PrintD2L(std::ostream & os) const {
-  if (correct > options.size()) {
-    emp::notify::Warning("Question ", id, ": Invalid answer.");
-  }
-
   os << "NewQuestion,MC,,,\n"
     << "ID,CSE231-" << id << ",,,\n"
     << "Title,,,,\n"
@@ -23,11 +22,62 @@ void Question::PrintD2L(std::ostream & os) const {
     << "Difficulty,1,,,\n"
     << "Image,,,,\n";
   for (size_t opt_id = 0; opt_id < options.size(); ++opt_id) {
-    os << "Option," << (opt_id == correct ? 100 : 0) << ","
-      << TextToD2L(options[opt_id]) << ",HTML,No feedback.\n";
+    os << "Option," << (opt_id == options[opt_id].is_correct ? 100 : 0) << ","
+      << TextToD2L(options[opt_id].text) << ",HTML,No feedback.\n";
   }
   os << "Hint,No hint for this one.,,,\n"
     << "Feedback,No feedback,HTML,,\n"
     << ",,,,\n"
     << ",,,,\n";
+}
+
+void Question::PrintHTML(std::ostream & os) const {
+  os << std::endl;
+}
+
+void Question::PrintLatex(std::ostream & os) const {
+  os << std::endl;
+}
+
+void Question::Validate() {
+  // Collect config info for this question.
+  correct_range = GetConfig(":correct", emp::Range<size_t>(1,1));
+  option_range = GetConfig(":options", emp::Range<size_t>(4,10));
+
+  // Are there enough correct answers?
+  const size_t correct_count = CountCorrect();
+  emp::notify::TestError(correct_count < correct_range.GetLower(),
+    "Question ", id, " has ", MakeCount(correct_count, "correct answer"), ", but ",
+     correct_range.GetLower(), " required.");
+  correct_range.LimitUpper(correct_count);    // Cannot have more correct answers than available.
+
+  // Are there too many REQUIRED correct answers?
+  const size_t correct_req_count = CountRequiredCorrect();
+  emp::notify::TestError(correct_req_count > correct_range.GetUpper(),
+    "Question ", id, " has ", MakeCount(correct_req_count, "correct answer"), "marked required, but at most ",
+     correct_range.GetUpper(), " allowed.");
+  correct_range.LimitLower(correct_req_count); // Cannot have few correct answers than required.
+
+  // Are there more REQUIRED options than allowed options?
+  const size_t required_count = CountRequired();
+  emp::notify::TestError(required_count > option_range.Upper(),
+    "Question ", id, " has ", MakeCount(required_count, "answer"), " marked required, but at most ",
+     option_range.GetUpper(), " options allowed.");
+  option_range.LimitLower(required_count);     // Must at least select required options.
+
+  // Are there enough available options?
+  const size_t incorrect_count = options.size() - correct_count;
+  const size_t max_options = incorrect_count + correct_range.Upper();
+  emp::notify::TestError(max_options < option_range.Lower(),
+    "Question ", id, " has a max of ", MakeCount(correct_range.Upper(), "correct answer"), " and ",
+    MakeCount(incorrect_count, "other option"), ", but requires at least ", option_range.Lower(),
+    "options.");
+  option_range.LimitUpper(max_options);     // Must at least select required options.
+}
+
+void Question::Generate(emp::Random & random) {
+  // Collect config info for this question (that wasn't collected in Validate(), above)
+  double alt_p = GetConfig(":alt_prob", 0.5);
+
+
 }
--- Question.hpp
+++ Question.hpp
@@ -4,7 +4,11 @@
 
 #include "emp/base/notify.hpp"
 #include "emp/base/vector.hpp"
+#include "emp/datastructs/map_utils.hpp"
 #include "emp/io/File.hpp"
+#include "emp/math/Random.hpp"
+#include "emp/math/random_utils.hpp"
+#include "emp/math/Range.hpp"
 #include "emp/tools/String.hpp"
 
 #include "functions.hpp"
@@ -13,35 +17,114 @@ using emp::String;
 
 class Question {
 private:
-  emp::String question;
-  emp::vector<emp::String> options;
-  size_t correct = (size_t) -1;
+  struct Option {
+    String text;       ///< Wording for this option.
+    bool is_correct;   ///< Is this option marked as a correct answer?
+    bool is_fixed;     ///< Is this option in a fixed position?
+    bool is_required;  ///< Does this option have to be included?
+  };
+
+  emp::String question;                  ///< Main wording for this questions.
+  emp::String alt_question;              ///< Toggled wording for this question.
+  emp::vector<Option> options;
   size_t id = (size_t) -1;
 
   emp::vector<String> base_tags;
   emp::vector<String> exclusive_tags;
   std::map<String,String> config_tags;
+
+  // Internal tracking
+  emp::Range<size_t> correct_range;
+  emp::Range<size_t> option_range;
+
+  enum class Section {
+    NONE=0,
+    QUESTION,
+    ALT_QUESTION,
+    OPTIONS
+  };
+  Section last_edit = Section::NONE;
+
+  template <typename T>
+  static emp::Range<T> StringToRange(String val) {
+    T val1 = val.Pop("-").As<T>();               // Respect a dash if there is one.
+    T val2 = val.size() ? val.As<T>() : val1;
+    return emp::MakeRange(val1, val2);
+  }
+
+  template <typename T>
+  T GetConfig(String name, T default_val=T{}) {
+    if (!emp::Has(config_tags, name)) return default_val;
+    const String & val = config_tags[name];
+
+    // Ranges should allow a dash.
+    if constexpr (std::is_same_v<T,emp::Range<size_t>>) {
+      return StringToRange<size_t>(val);
+    }
+    else if constexpr (std::is_same_v<T,emp::Range<double>>) {
+      return StringToRange<double>(val);
+    }
+    else {
+      return val.As<T>();
+    }
+  }
+
 public:
-  Question(size_t _id) : id(_id) { };
+  Question() { }
+  Question(size_t _id) : id(_id) { }
   Question(const Question &) = default;
   Question(Question &&) = default;
 
   Question & operator=(const Question &) = default;
   Question & operator=(Question &&) = default;
 
+  size_t CountCorrect() const {
+    return std::count_if(options.begin(), options.end(),
+                         [](const Option & o){ return o.is_correct; });
+  }
+
+  size_t CountRequired() const {
+    return std::count_if(options.begin(), options.end(),
+                         [](const Option & o){ return o.is_required; });
+  }
+
+  size_t CountRequiredCorrect() const {
+    return std::count_if(options.begin(), options.end(),
+                         [](const Option & o){ return o.is_required && o.is_correct; });
+  }
+
   void AddText(const emp::String & line) {
-    // If we have answer options, append it to the most recent one, else it is the question.
-    if (options.size()) {
-      options.back().Append('\n', line);
-    } else {
-      if (question.size()) question += '\n';
-      question += line;
+    // Text with a start symbol would have been directed elsewhere.  Regular text is either a
+    // question or an extension of the last thing being written.
+    switch (last_edit) {
+    case Section::NONE:
+      question = line;
+      last_edit = Section::QUESTION;
+      break;
+    case Section::QUESTION:
+      question.Append('\n', line);
+      break;
+    case Section::ALT_QUESTION:
+      alt_question.Append('\n', line);
+      break;
+    case Section::OPTIONS:      
+      options.back().text.Append('\n', line);
     }
   }
 
+  void AddAltQuestion(const emp::String & line) {
+    alt_question = line;
+    last_edit = Section::ALT_QUESTION;    
+  }
+
   void AddOption(emp::String tag, const emp::String & option) {
-    if (tag[0] == '[') correct = options.size();
-    options.push_back(option);
+    options.push_back(
+      Option{option,            // Option text.
+            (tag[0] == '['),    // Is it correct?
+            tag.Has('>'),       // Is it in a fixed position?
+            tag.Has('+')        // Is it required?
+            });      
+      last_edit = Section::OPTIONS;
   }  
 
   void AddTags(String line) {
@@ -63,4 +146,9 @@ public:
   }
   void Print(std::ostream & os=std::cout) const;
   void PrintD2L(std::ostream & os=std::cout) const;
+  void PrintHTML(std::ostream & os=std::cout) const;
+  void PrintLatex(std::ostream & os=std::cout) const;
+
+  void Validate();
+  void Generate(emp::Random & random);
 };
