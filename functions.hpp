ba35f9d0d01131f7d0fef2e6094a8eded39df2ed
Charles Ofria <charles.ofria@gmail.com>
2023-08-08 09:28:27 -0400

    Created TextToHTML() function.

--- functions.hpp
+++ functions.hpp
@@ -115,3 +115,61 @@ static inline emp::String TextToLatex(const emp::String & text) {
   for (auto & line : lines) line = LineToLatex(line);
   return emp::Join(lines, "\\\\\n");
 }
+
+
+static inline emp::String LineToHTML(emp::String line) {
+  emp::notify::TestError(line.Has('\n'), "Newline found inside of line: ", line);
+  emp::String out_line;
+
+  bool in_codeblock = line.HasPrefix("    ");
+  bool in_code = in_codeblock;
+
+  if (in_codeblock) {
+    line.PopFixed(4);
+    out_line += "&nbsp;&nbsp;<code>";
+
+    size_t ws_count = 0;
+    while (ws_count < line.size() && line[ws_count] == ' ') ws_count++;
+    if (ws_count) {
+      out_line += emp::MakeRepeat("&nbsp;", ws_count);
+      line.PopFixed(ws_count);
+    }
+  }
+
+  for (char c : line) {
+    switch (c) {
+      case '&': out_line += "&amp;";  break;
+      case '<': out_line += "&lt;"; break;
+      case '>': out_line += "&gt;"; break;
+      case '\'': out_line += "&apos;"; break;
+      case '"': out_line += "&quot;"; break;
+
+      // Replace ` with \texttt{ or }
+      case '`':
+        if (in_codeblock) {
+          out_line += '`';
+        } else {
+          if (in_code) out_line += "</code>";
+          else out_line += "<code>";
+          in_code = !in_code;
+        }
+        break;
+
+      default:
+        out_line += c;
+        break;
+    }
+  }
+
+  // If we are in code at the end of the entry, close it off.
+  if (in_code) out_line += "</code>";
+
+  return out_line;
+}
+
+// Convert a whole text block to HTML format.
+static inline emp::String TextToHTML(const emp::String & text) {
+  emp::vector<emp::String> lines = text.Slice("\n");
+  for (auto & line : lines) line = LineToHTML(line);
+  return emp::Join(lines, "\\\\\n");
+}
